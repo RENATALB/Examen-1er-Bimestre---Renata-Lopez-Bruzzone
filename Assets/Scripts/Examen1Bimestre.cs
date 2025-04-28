6a41b48f1e117e12f53fdaf31d73d5914b13f983
RENATALB <166546709+RENATALB@users.noreply.github.com>
2025-04-28 10:19:39 -0300

    Initial Commit 2

--- Assets/Scripts/Examen1Bimestre.cs
+++ Assets/Scripts/Examen1Bimestre.cs
@@ -7,12 +7,10 @@ public class Examen1Bimestre : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        // 1. Declarar las variables: Veo en la consigna y me fijo que cosas me conviene tener en una variable. EJ HELADERÍA: Todo lo que el usuario debe ingresar: Gustos, cantidad. Afuera de la función, únicamente se declaran las variables.
+        // 2. Validar los datos ingresados. Fijarse si los datos están bien o mal, buscar errores.
+        // 3. Procesamiento de datos:
+        // 4. Mostrar los resultados
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
