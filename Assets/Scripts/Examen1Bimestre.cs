35f2e1522185b19c1e4eb99c0411b0fb74264e2b
RENATALB <166546709+RENATALB@users.noreply.github.com>
2025-04-28 11:47:22 -0300

    litros por dia

--- Assets/Scripts/Examen1Bimestre.cs
+++ Assets/Scripts/Examen1Bimestre.cs
@@ -5,31 +5,36 @@ using UnityEngine;
 public class Examen1Bimestre : MonoBehaviour
 {
     public float kilometrosARecorrer;
-    int costo1Litro = 130;
     public int combustibleCargado;
+    int litrosPorDia = 6; // 90km dividido 15km
+    int costo1Litro = 130;
     int periodoDeDias;
     int cantidadDeUnidades;
+
     float costoTotalCombustible;
     float porcentajeDescuento;
 
     void Start()
     {
-        // 1. Declarar las variables: Veo en la consigna y me fijo que cosas me conviene tener en una variable. EJ HELADERÍA: Todo lo que el usuario debe ingresar: Gustos, cantidad. Afuera de la función, únicamente se declaran las variables.
-        // 2. Validar los datos ingresados. Fijarse si los datos están bien o mal, buscar errores.
-        
         if (periodoDeDias < 5 || cantidadDeUnidades < 1)
         {
             Debug.Log("Cantidad inválida");
             return;
         }
-        if (combustibleCargado >= 100)
+
+        if (combustibleCargado < 100)
         {
-            porcentajeDescuento = 20;
+            porcentajeDescuento = 0;
+            costoTotalCombustible = litrosPorDia * costo1Litro * (1 - porcentajeDescuento);
+        }
+
+        else if (combustibleCargado >= 100)
+        {
+            porcentajeDescuento = 0.2f;
+            costoTotalCombustible = litrosPorDia * costo1Litro * (1 - porcentajeDescuento);
         }
 
 
-        // 3. Procesamiento de datos:
-        // 4. Mostrar los resultados
-        // Una flota de [cantidadDeUnidades] unidades trabajando durante [periodoDeDias] días implicará un gasto de [costoTotalCombustible] pesos en concepto de combustible
+        Debug.Log("Una flota de "+ cantidadDeUnidades + "unidades trabajando durante " + periodoDeDias + "días implicará un gasto de "+ costoTotalCombustible + "pesos en concepto de combustible");
     }
 }
